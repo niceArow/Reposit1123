d067f9c397c848f70e8a7f147ddf884349bed8b5
niceArow <107623104+niceArow@users.noreply.github.com>
2022-06-16 18:46:52 +0700

    Add files via upload

---
+++ прога302/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+class Program
+{
+    public static byte rdmeth()
+    {
+        RNGCryptoServiceProvider Rand = new RNGCryptoServiceProvider();
+        byte[] rndnum = new byte[1];
+        Rand.GetBytes(rndnum);
+        return (rndnum[0]);
+    }
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            double val = rdmeth();
+            Console.Write(val);
+            Console.Write(" ");
+        }
+        Console.ReadLine();
+    }
+}
